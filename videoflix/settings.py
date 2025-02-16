bb7423caf37bb6ec8f1c1772c1e76073b54a88a6
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-02-16 19:00:58 +0100

    bug fix settings

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -35,17 +35,8 @@ DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1",
     "localhost", '34.65.108.156','gc.silvanstuber.ch']
 
-<<<<<<< Updated upstream
-USE_X_FORWARDED_HOST = False
-USE_X_FORWARDED_PORT = True
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-=======
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
->>>>>>> Stashed changes
 
 # Application definition
 
