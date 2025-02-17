f980f954f1ebbd93d37da8bf26a2aa1183b79312
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-02-17 21:47:53 +0100

    bugfix content_app tasks.py

--- content_app/tasks.py
+++ content_app/tasks.py
@@ -1,13 +1,32 @@
 import subprocess
 from pathlib import Path
 
+
 def convert_720p(source):
     new_file_name = f"{source[:-4]}_720p.mp4"
-    cmd = 'ffmpeg -y -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-    run =   subprocess.run(cmd, check=True)
+    cmd = ['/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd720', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2', new_file_name]
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"Video erfolgreich konvertiert zu: {new_file_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler bei der Konvertierung des Videos: {e}")
+    except FileNotFoundError:
+        print("ffmpeg nicht gefunden. Bitte stelle sicher, dass ffmpeg korrekt installiert ist.")
+    except Exception as e:
+        print(f"Ein unbekannter Fehler ist aufgetreten: {e}")
+
 
 def convert_480p(source):
     new_file_name = f"{source[:-4]}_480p.mp4"
-    cmd = 'ffmpeg -y -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-    run =   subprocess.run(cmd, check=True)
+    cmd = ['/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd480', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2', new_file_name]
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"Video erfolgreich konvertiert zu: {new_file_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler bei der Konvertierung des Videos: {e}")
+    except FileNotFoundError:
+        print("ffmpeg nicht gefunden. Bitte stelle sicher, dass ffmpeg korrekt installiert ist.")
+    except Exception as e:
+        print(f"Ein unbekannter Fehler ist aufgetreten: {e}")
+
 
