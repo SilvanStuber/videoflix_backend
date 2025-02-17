271ce1b7900a81dcf92f43089ac54074f1de7f1f
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-02-17 22:14:17 +0100

    tasks.py performance optimization for server

--- content_app/tasks.py
+++ content_app/tasks.py
@@ -4,7 +4,13 @@ from pathlib import Path
 
 def convert_720p(source):
     new_file_name = f"{source[:-4]}_720p.mp4"
-    cmd = ['/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd720', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2', new_file_name]
+    cmd = [
+        '/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd720', 
+        '-c:v', 'libx264', '-crf', '28', 
+        '-c:a', 'aac', '-strict', '-2', 
+        '-threads', '1', 
+        new_file_name
+    ]
     try:
         subprocess.run(cmd, check=True)
         print(f"Video erfolgreich konvertiert zu: {new_file_name}")
@@ -18,7 +24,13 @@ def convert_720p(source):
 
 def convert_480p(source):
     new_file_name = f"{source[:-4]}_480p.mp4"
-    cmd = ['/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd480', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2', new_file_name]
+    cmd = [
+        '/usr/bin/ffmpeg', '-y', '-i', source, '-s', 'hd480', 
+        '-c:v', 'libx264', '-crf', '28', 
+        '-c:a', 'aac', '-strict', '-2', 
+        '-threads', '1',  
+        new_file_name
+    ]
     try:
         subprocess.run(cmd, check=True)
         print(f"Video erfolgreich konvertiert zu: {new_file_name}")
@@ -28,5 +40,3 @@ def convert_480p(source):
         print("ffmpeg nicht gefunden. Bitte stelle sicher, dass ffmpeg korrekt installiert ist.")
     except Exception as e:
         print(f"Ein unbekannter Fehler ist aufgetreten: {e}")
-
-
